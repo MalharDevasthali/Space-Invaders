17dbc83247c53c35cb137c61f8a2554958c8d30d
Malhar <malhardevasthali007@gmail.com>
2024-07-05 10:00:57 +0530

    Added Player Spawning Logic

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -25,13 +25,50 @@ public:
     {
 
     }
+    sf::Vector2f getPosition()
+    {
+        return position;
+    }
 
 
 };
 
-int main() {
+int main()
+{
 
     Player player;
 
-    return 0;
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+    sf::RenderWindow window(videoMode, "SFML Window");
+
+    player.player_texture.loadFromFile("assets/textures/player_ship.png");
+    player.player_sprite.setTexture(player.player_texture);
+
+    while (window.isOpen()) 
+    {
+        sf::Event event;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            player.move();
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            player.move();
+        }
+
+        window.clear(sf::Color::Black);
+        player.player_sprite.setPosition(player.getPosition()); 
+        window.draw(player.player_sprite);   
+        window.display(); 
+
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        return 0;
+    }
 }
