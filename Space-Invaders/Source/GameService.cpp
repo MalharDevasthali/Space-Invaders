20286d1e1b1bf368542ff711b6c87f1dd3d432e8
Malhar <malhardevasthali007@gmail.com>
2024-07-08 11:25:23 +0530

    Added GameService

---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,34 @@
+#include"../Header/GameService.h"
+
+void GameService::initialize()
+{
+}
+
+void GameService::destroy()
+{
+}
+
+GameService::GameService()
+{
+}
+
+GameService::~GameService()
+{
+}
+
+void GameService::ignite()
+{
+}
+
+void GameService::update()
+{
+}
+
+void GameService::render()
+{
+}
+
+bool GameService::isRunning()
+{
+	return false;
+}
