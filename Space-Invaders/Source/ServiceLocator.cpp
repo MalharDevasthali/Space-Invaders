447938099d2dc63531a7f92ada44396a3ea1e9ed
Malhar <malhardevasthali007@gmail.com>
2024-07-10 09:00:00 +0530

    Bug Fix - Window

--- Space-Invaders/Source/ServiceLocator.cpp
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -1,48 +1,43 @@
 #include "../Header/ServiceLocator.h"
 
-// Constructor: Initializes the graphic_service pointer to null and creates services.
 ServiceLocator::ServiceLocator() {
-	 graphic_service = nullptr; // Initialize graphic_service to null
-	createServices(); // Call createServices to instantiate services
+	 graphic_service = nullptr; 
+	 event_service = nullptr;
+	createServices(); 
 }
 
-// Destructor: Cleans up resources by clearing all services.
 ServiceLocator::~ServiceLocator() {
-	clearAllServices(); // Call clearAllServices to delete any allocated services	
+	clearAllServices(); 	
 }
 
-// Creates service instances, specifically the graphic service in this case.
 void ServiceLocator::createServices() {
-	graphic_service = new GraphicService(); // Dynamically create a GraphicService instance
+	graphic_service = new GraphicService(); 
+	event_service = new EventService();
 }
 
-// Deletes allocated services to prevent memory leaks, specifically the graphic service.
 void ServiceLocator::clearAllServices() {
-	delete(graphic_service); // Delete the graphic_service instance
-	graphic_service = nullptr; // Reset pointer to null to avoid dangling pointer
+	delete(graphic_service);
+	delete(event_service);
 }
 
-// Returns a pointer to ServiceLocator.
 ServiceLocator* ServiceLocator::getInstance() {
-	static ServiceLocator instance; // we will discuss what 'static' means at a later time.
-	return &instance; // Return address of the instance
+	static ServiceLocator instance; 
+	return &instance; 
 }
 
-// Calls initialize on the graphic service, readying it for use.
 void ServiceLocator::initialize() {
-	graphic_service->initialize(); // Initialize graphic service
+	graphic_service->initialize(); 
+	event_service->initialize();
 }
 
-// Updates the state of the graphic service.
 void ServiceLocator::update() {
-	graphic_service->update(); // Update graphic service
+	graphic_service->update(); 
+	event_service->update();
 }
 
-// Renders using the graphic service.
 void ServiceLocator::render() {
-	graphic_service->render(); // Render graphic service
+	graphic_service->render();
 }
 
-// Returns a pointer to the currently set graphic service.
 GraphicService* ServiceLocator::getGraphicService() { return graphic_service; }
 EventService* ServiceLocator::getEventService() { return event_service; }
