d5f7aab74aeb16db9c737ecb5ec972a8eeebb05f
Malhar <malhardevasthali007@gmail.com>
2024-07-16 09:45:57 +0530

    Resolution Changed to Full Screen

--- Space-Invaders/Source/UI/MainMenu/MainMenuController.cpp
+++ Space-Invaders/Source/UI/MainMenu/MainMenuController.cpp
@@ -25,6 +25,77 @@ namespace UI
 
         void MainMenuController::render()
         {
+            game_window->draw(background_sprite);
+            game_window->draw(play_button_sprite);
+            game_window->draw(instructions_button_sprite);
+            game_window->draw(quit_button_sprite);
+        }
+
+        void MainMenuController::initializeBackgroundImage()
+        { 
+            if (background_texture.loadFromFile(background_texture_path))
+            {   
+                background_sprite.setTexture(background_texture);
+                scaleBackgroundImage();
+            }
+        }
+
+        void MainMenuController::scaleBackgroundImage()
+        { 
+            background_sprite.setScale(
+                static_cast<float>(game_window->getSize().x) / background_sprite.getTexture()->getSize().x,
+                static_cast<float>(game_window->getSize().y) / background_sprite.getTexture()->getSize().y
+            );
+        }
+
+        void MainMenuController::initializeButtons()
+        {
+        
+            if (loadButtonTexturesFromFile())
+            {
+                setButtonSprites();
+                scaleAllButttons();
+                positionButtons();
+            }
+        }
+
+        bool MainMenuController::loadButtonTexturesFromFile()
+        {
+            return play_button_texture.loadFromFile(play_button_texture_path) &&
+                instructions_button_texture.loadFromFile(instructions_button_texture_path) &&
+                quit_button_texture.loadFromFile(quit_button_texture_path);
+        }
+
+        void MainMenuController::setButtonSprites()
+        {
+            play_button_sprite.setTexture(play_button_texture);
+            instructions_button_sprite.setTexture(instructions_button_texture);
+            quit_button_sprite.setTexture(quit_button_texture);
+        }
+
+
+        void MainMenuController::scaleAllButttons()
+        {
+            scaleButton(&play_button_sprite);
+            scaleButton(&instructions_button_sprite);
+            scaleButton(&quit_button_sprite);
+        }
+
+        void MainMenuController::scaleButton(sf::Sprite* button_to_scale)
+        {
+            button_to_scale->setScale(
+                button_width / button_to_scale->getTexture()->getSize().x,
+                button_height / button_to_scale->getTexture()->getSize().y
+            );
+        }
+
+        void MainMenuController::positionButtons()
+        {
+            float x_position = (static_cast<float>(game_window->getSize().x) / 2) - button_width / 2;
+
+            play_button_sprite.setPosition({ x_position, 500.f });
+            instructions_button_sprite.setPosition({ x_position, 700.f });
+            quit_button_sprite.setPosition({ x_position, 900.f });
         }
 
     }
